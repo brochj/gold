c699f7a6f445ddfc41055100f8ec92e86afda726
Oscar Broch <brochj@gmail.com>
2019-09-22 19:48:40 -0300

    Add Reactotron + Redux + Saga

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-// import '~/config/ReactotronConfig';
+import './config/ReactotronConfig';
 
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store';
 import Routes from '~/routes';
 
 export default function src() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Routes />
+      </PersistGate>
+    </Provider>
+  );
 }
---
+++ mobile/src/store/modules/auth/sagas.js
@@ -0,0 +1,65 @@
+import { Alert } from 'react-native';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+
+import api from '~/services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (!user.provider) {
+      Alert.alert('Error', 'Usuário não é prestador');
+      yield put(signFailure());
+      return;
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    // put é para disparar actions
+    yield put(signInSuccess(token, user));
+
+    // history.push('/dashboard');
+  } catch (eer) {
+    Alert.alert('Error', 'Falha na autenticação, verifique seus dados');
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', { name, email, password, provider: true });
+
+    // history.push('/');
+  } catch (err) {
+    Alert.alert('Error', 'Falha no cadastro, verifique seus dados');
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+// all é pra ficar ouvindo as actions
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
